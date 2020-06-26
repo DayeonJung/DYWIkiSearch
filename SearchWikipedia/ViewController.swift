a34f5b9367561a43acd496829e1f1e8d7e880992
DayeonJung <ekdus0227@gmail.com>
2020-06-26 14:10:29 +0900

    Initial Commit

---
+++ SearchWikipedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchWikipedia
+//
+//  Created by nolbal_dayeon on 2020/06/26.
+//  Copyright © 2020 dayeonJung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
